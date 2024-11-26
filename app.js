1459abf33c85cfebae29a8779012c0b02b59ee91
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-11-26 12:37:00 +0300

    Generate QR code from user input

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 "use strict";
 import { input } from "@inquirer/prompts";
 import qr from "qr-image";
-
+import fs from "node:fs";
 const userInput = await input({ message: "Enter a text to generate qr-code" });
 const userInputToFileName =
   userInput.toLocaleLowerCase().split(" ").join("_") + ".svg";
+
+const qr_svg = qr.image(userInput, {
+  type: "svg",
+});
+
+qr_svg.pipe(fs.createWriteStream(userInputToFileName));
+
+const svg_string = qr.imageSync(userInput, { type: "svg" });
